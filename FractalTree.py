c3e2b4f56284cc140c9eae1e4f79d19746e62c76
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2020-12-25 22:32:28 +0000

    refactored to use turtle

--- FractalTree.py
+++ FractalTree.py
@@ -1,37 +1,29 @@
-from tkinter import *
-import math
-
-canvas = Canvas(width=1000, height=800, bg='black')
-canvas.pack(expand=YES, fill=BOTH)
+import turtle
 
 
-#canvas.create_line(XStart, YStart, XEnd, YEnd, width, fill)
 
-def DrawBranch(startX, startY, angle, length, width):
-    #print(str(startX) + "  " + str(startY) + "    " + str(width))
-    angleRads = angle * math.pi / 180
-    endX = startX + length * math.cos(angleRads)
-    endY = startY + length * math.sin(angleRads)
-    angle1, angle2 = angle, angle
-    length1, length2 = length, length
-    offset = -35
+def DrawBranch(branchLen):
 
-    if (length > 15):
-        canvas.create_line(startX, startY, endX, endY, width=width, fill='green')
-
-        DrawBranch(endX,endY, angle1+offset, length*0.5, width*0.75)
-
-        endX = startX + length * math.cos(angleRads)
-        endY = startY + length * math.sin(angleRads)
-
-        DrawBranch(endX,endY, angle2-offset, length*0.5, width*0.75)
-
-        endX = startX + length * math.cos(angleRads)
-        endY = startY + length * math.sin(angleRads)
-
-        DrawBranch(endX,endY, 270, length*0.8, width*0.75)
+    if branchLen > 10:
+        t.forward(branchLen)
+        t.right(40)
+        DrawBranch(branchLen-15)
+        t.left(80)
+        DrawBranch(branchLen-15)
+        t.right(40)
+        t.backward(branchLen)
 
 
-#Recusion Start
-DrawBranch(500,475,270,75,10)
-mainloop()
+
+s = turtle.Screen()
+s.bgcolor("black")
+
+t = turtle.Turtle()
+t.color("green")
+t.speed(5000)
+
+t.width(5)
+t.left(90)
+
+DrawBranch(100)
+turtle.done()
