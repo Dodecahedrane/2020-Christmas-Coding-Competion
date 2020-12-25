95b75208b15b6add78b7a181f0e6511e3dc63866
Oliver Guy <oliver.guy-1@students.plymouth.ac.uk>
2020-12-25 11:05:53 +0000

    Update

--- FractalTree.py
+++ FractalTree.py
@@ -3,17 +3,21 @@ from tkinter import *
 canvas = Canvas(width=1000, height=500, bg='black')
 canvas.pack(expand=YES, fill=BOTH)
 
+#canvas.create_line(XStart, YStart, XEnd, YEnd, width, fill)
+
+def DrawBranch(start,angle,length, width):
+
+    
+    if (width > 0):
+        canvas.create_line(500, 480, 500, 450, width, fill='green')
 
 
-
-#def DrawBranch():
 
 
 
 #Draw main trunk
-# (XStart, YStart, XEnd, YEnd)
 canvas.create_line(500, 480, 500, 450, width=10, fill='green')
 #Initial Params
-#DrawBranch():
+DrawBranch():
 
 mainloop()
