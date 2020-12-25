14e8e67d4ba9f89e5f64a7f2f92cba7893708e43
Oliver Guy <oliver.guy-1@students.plymouth.ac.uk>
2020-12-25 02:13:58 +0000

    Can draw on canvas

--- FractalTree.py
+++ FractalTree.py
@@ -1,10 +1,19 @@
 from tkinter import *
 
-canvas = Canvas(width=920, height=480, bg='black')
+canvas = Canvas(width=1000, height=500, bg='black')
 canvas.pack(expand=YES, fill=BOTH)
 
-# (XStart, YStart, XEnd, YEnd)
-canvas.create_line(100, 10,200, 10, width=10, fill='green')
 
+
+
+#def DrawBranch():
+
+
+
+#Draw main trunk
+# (XStart, YStart, XEnd, YEnd)
+canvas.create_line(500, 480, 500, 450, width=10, fill='green')
+#Initial Params
+#DrawBranch():
 
 mainloop()
