bfa43a7b39953e2180ca715b14a01072693a549e
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2020-12-25 14:30:47 +0000

    kinda looking tree ish

--- FractalTree.py
+++ FractalTree.py
@@ -1,29 +1,32 @@
 from tkinter import *
 import math
 
-canvas = Canvas(width=1000, height=500, bg='black')
+canvas = Canvas(width=1400, height=800, bg='black')
 canvas.pack(expand=YES, fill=BOTH)
 
 
 #canvas.create_line(XStart, YStart, XEnd, YEnd, width, fill)
 
 def DrawBranch(startX, startY, angle, length, width):
-
+    #print(str(startX) + "  " + str(startY) + "    " + str(width))
     angleRads = angle * math.pi / 180
-    endX = startX + width * math.cos(angleRads)
-    endY = startY + width * math.sin(angleRads)
+    endX = startX + length * math.cos(angleRads)
+    endY = startY + length * math.sin(angleRads)
 
 
-    if (width > 0):
+    if (length > 5):
         canvas.create_line(startX, startY, endX, endY, width=width, fill='green')
-        DrawBranch(endX,endY, angle-5,length-1,width-0.25)
+        DrawBranch(endX,endY, angle+5,length*0.8,width*0.85)
+        endX = startX + width * math.cos(angleRads)
+        endY = startY + width * math.sin(angleRads)
+        DrawBranch(endX,endY, 90-angle+5,length*0.8,width*0.85)
 
 
 
 
 #Draw main trunk
 canvas.create_line(500, 480, 500, 450, width=10, fill='green')
-#Initial Params
-DrawBranch(500,450,45,10,10)
+#Recusion Start
+DrawBranch(500,450,270,100,10)
 
 mainloop()
